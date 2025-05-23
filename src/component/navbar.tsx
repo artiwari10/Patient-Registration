3c84133ddf02e3b616eaf159980bac002d9364cd
Anmol Ratan Tiwari <anmolratantiwari62@gmail.com>
2025-05-23 10:16:12 -0700

    Fixing Deployment issue

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -4,7 +4,7 @@ import { UserPlus, Search } from 'lucide-react';
 
 
 
-const Navbar: React.FC<> = () => {
+const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-900 border-b border-gray-800 shadow-md">
       <div className="max-w-7xl lg:max-w-full mx-auto px-4 sm:px-6 lg:px-8">
